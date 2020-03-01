e67b581ac8c54eb5f901ffe65d9c838f9be9e506
Joel Kennedy <joelkennedy@sky.com>
2020-03-01 18:31:22 +0000

    Bump version

--- src/thisdb.js
+++ src/thisdb.js
@@ -4,7 +4,7 @@ var ThisDB = function(initArgs) {
   var defaultResponseFormat = initArgs.defaultResponseFormat || 'text';
 
   var endpoint = 'https://api.thisdb.com/v1/';
-  var version = '0.1.0';
+  var version = '0.1.1';
 
   var headerMap = {};
 
